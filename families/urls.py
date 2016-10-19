c75e4365244b8ee8208a10fb130b1f2a71ecd4c9
Ryan Goins <ryan.c.goins@gmail.com>
2016-10-19 07:52:04 -0400

    got basic meal form working

--- families/urls.py
+++ families/urls.py
@@ -2,14 +2,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from . import views
-from families.views import CreateGroup, CreateMeal
+from families.views import CreateGroup
 from todo.views import view_list
 from . import models
 
 urlpatterns = [
     url(r'(?P<group_pk>\d+)/meals/(?P<meal_pk>\d+)/$', views.meal_detail, name='meal_detail'),
     url(r'(?P<group_pk>\d+)/meals/$', views.meal_list, name='meal_list'),
-    url(r'(?P<group_pk>\d+)/meals/create/$', CreateMeal.as_view(), name='create_meal'),
+    url(r'(?P<group_pk>\d+)/meals/create/$', views.create_meal, name='create_meal'),
     url(r'(?P<group_pk>\d+)/$', views.group_detail, name='detail'),
     url(r'^user/(?P<username>[\w.@+-]+)/$', views.group_list, name='group_list'),
     url(r'^create/$', CreateGroup.as_view(), name='create_group'),
